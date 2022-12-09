460337556c8403cabd72169cfa5cbcc5b9f131e2
Simone Vittori <hello@simonewebdesign.it>
2022-12-09 10:34:32 +0000

    Update cht.fish

--- .config/fish/functions/cht.fish
+++ .config/fish/functions/cht.fish
@@ -1,3 +1,10 @@
+# retrieve command cheat sheets from cheat.sh
+# fish version by @tobiasreischmann
+# Originally taken from https://github.com/chubin/cheat.sh/blob/master/share/fish.txt
+
 function cht
-  curl "cheat.sh/$argv"
+    curl cheat.sh/$argv
 end
+
+# register completions (on-the-fly, non-cached, because the actual command won't be cached anyway
+complete -c cht -xa '(curl -s cheat.sh/:list)'
